811e1d10f4deb8d8178c6579f2e7362cf33236ec
TECHkoumu <nariyuki08@gmail.com>
2023-03-28 21:39:49 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,24 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+  end
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    #チャットルームに紐づいたメッセージのインスタンス生成。message_paramsを引数にしてprivateメソッド呼び出し。
+    if @message.save
+      redirect_to room_messages_path(@room)
+      #@roomで指定したroom_messages_pathへリダイレクト。
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+    #messageキー(モデル)探す→contentキーを許可→許可するパラメータにcurrent_user.idも追加
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to:"rooms#index" #ルートパスへのアクセス時、rooms_controllerのindexアクション呼び出す
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only:[:index, :create]
+  end
 end
